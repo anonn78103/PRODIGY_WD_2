16f4759c894946c305555a776a4db646689ebd52
anonn78103 <139428061+anonn78103@users.noreply.github.com>
2025-02-18 11:04:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
+const timerDisplay = document.getElementById("timerDisplay");
+const toggleBtn = document.getElementById("toggleBtn");
+const actionBtn = document.getElementById("actionBtn");
+const lapsContainer = document.getElementById("lapsContainer");
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_IvcFGAMj0p_lV4C762wacN-t1R2YyGBPitv4yCCR1gGhPUr2el6Cjs&s">
-    <meta name="description" content="An elegant timer that tracks time, pauses, and records laps." />
-    <meta name="keywords" content="timer, stopwatch, track time, laps" />
-    <title>Timer</title>
-    <link rel="stylesheet" href="style.css" />
-</head>
+let timer;
+let running = false;
+let timeElapsed = 0;
 
-<body>
-    <div>
-        <div>
-            <h1>Timer</h1>
-            <span class="icon-wrapper">
-                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"
-                        fill="#CCCCCC">
-                        <path
-                            d="M390-860q-12.75 0-21.37-8.68-8.63-8.67-8.63-21.5 0-12.82 8.63-21.32 8.62-8.5 21.37-8.5h180q12.75 0 21.38 8.68 8.62 8.67 8.62 21.5 0 12.82-8.62 21.32-8.63 8.5-21.38 8.5H390Zm90.18 447q12.82 0 21.32-8.63 8.5-8.62 8.5-21.37v-170q0-12.75-8.68-21.38-8.67-8.62-21.5-8.62-12.82 0-21.32 8.62-8.5 8.63-8.5 21.38v170q0 12.75 8.68 21.37 8.67 8.63 21.5 8.63ZM480-81q-74 0-139.5-28.5T226-187q-49-49-77.5-114.5T120-441q0-74 28.5-139.5T226-695q49-49 114.5-77.5T480-801q67 0 125.5 22T710-717l30-30q9-9 21.5-9t21.5 9q9 9 9 21.5t-9 21.5l-30 30q36 40 61.5 97T840-441q0 74-28.5 139.5T734-187q-49 49-114.5 77.5T480-81Z" />
-                    </svg></a>
-            </span>
-        </div>
+function padDigits(num, length = 2) {
+    return String(num).padStart(length, "0");
+}
 
-        <div class="display" id="timerDisplay">00:00:00</div>
-        <ul class="recorded-laps" id="lapsContainer"></ul>
+function formatElapsedTime(time) {
+    const mins = Math.floor(time / 60000);
+    const secs = Math.floor((time % 60000) / 1000);
+    const millisecs = Math.floor((time % 1000) / 10);
+    return `${padDigits(mins)}:${padDigits(secs)}:${padDigits(millisecs)}`;
+}
 
-        <div class="btn-group">
-            <button id="toggleBtn" class="btn btn-round btn-start">
-                <svg id="toggleIcon" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960"
-                    width="40px" fill="#0000F5">
-                    <path
-                        d="M320-263v-438q0-15 10-24.17 10-9.16 23.33-9.16 4.34 0 8.84 1.16 4.5 1.17 8.83 3.5L715.67-510q7.66 5.33 11.5 12.33 3.83 7 3.83 15.67t-3.83 15.67q-3.84 7-11.5 12.33L371-234.33q-4.33 2.33-8.83 3.5-4.5 1.16-8.84 1.16-13.33 0-23.33-9.16Q320-248 320-263Z" />
-                </svg>
-            </button>
-            <button id="actionBtn" class="btn btn-round btn-lap">
-                <svg id="actionIcon" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960"
-                    width="40px" fill="#0000F5">
-                    <path
-                        d="M240-306.67v-346.66q0-27.5 19.58-47.09Q279.17-720 306.67-720h346.66q27.5 0 47.09 19.58Q720-680.83 720-653.33v346.66q0 27.5-19.58 47.09Q680.83-240 653.33-240H306.67q-27.5 0-47.09-19.58Q240-279.17 240-306.67Z" />
-                </svg>
-            </button>
-        </div>
-    </div>
-    <script src="script.js"></script>
-</body>
+function refreshTimer() {
+    timerDisplay.textContent = formatElapsedTime(timeElapsed);
+}
 
-</html>
+function beginTimer() {
+    const start = Date.now() - timeElapsed;
+    timer = setInterval(() => {
+        timeElapsed = Date.now() - start;
+        refreshTimer();
+    }, 100);
+    running = true;
+}
+
+function haltTimer() {
+    clearInterval(timer);
+    running = false;
+}
+
+function logLap() {
+    const lapTime = formatElapsedTime(timeElapsed);
+    const lapIndex = lapsContainer.children.length + 1;
+    const lapElement = document.createElement("li");
+    lapElement.className = "lap-entry";
+    lapElement.innerHTML = `<span class="lap-index">#${lapIndex}</span><span class="lap-time">${lapTime}</span>`;
+    lapsContainer.appendChild(lapElement);
+}
+
+function resetTimer() {
+    haltTimer();
+    timeElapsed = 0;
+    refreshTimer();
+    lapsContainer.innerHTML = "";
+}
+
+toggleBtn.addEventListener("click", () => {
+    if (running) {
+        haltTimer();
+        toggleBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#0000F5">
+                <path d="M320-263v-438q0-15 10-24.17 10-9.16 23.33-9.16 4.34 0 8.84 1.16 4.5 1.17 8.83 3.5L715.67-510q7.66 5.33 11.5 12.33 3.83 7 3.83 15.67t-3.83 15.67q-3.84 7-11.5 12.33L371-234.33q-4.33 2.33-8.83 3.5-4.5 1.16-8.84 1.16-13.33 0-23.33-9.16Q320-248 320-263Z"/>
+            </svg>
+        `;
+        actionBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#0000F5">
+                <path d="M240-306.67v-346.66q0-27.5 19.58-47.09Q279.17-720 306.67-720h346.66q27.5 0 47.09 19.58Q720-680.83 720-653.33v346.66q0 27.5-19.58 47.09Q680.83-240 653.33-240H306.67q-27.5 0-47.09-19.58Q240-279.17 240-306.67Z"/>
+            </svg>
+        `;
+    } else {
+        beginTimer();
+        toggleBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#0000F5">
+                <path d="M320-234h120v-492H320v492Zm200 0h120v-492H520v492Z"/>
+            </svg>
+        `;
+        actionBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#0000F5">
+                <path d="M240-306.67v-346.66q0-27.5 19.58-47.09Q279.17-720 306.67-720h346.66q27.5 0 47.09 19.58Q720-680.83 720-653.33v346.66q0 27.5-19.58 47.09Q680.83-240 653.33-240H306.67q-27.5 0-47.09-19.58Q240-279.17 240-306.67Z"/>
+            </svg>
+        `;
+    }
+});
+
+actionBtn.addEventListener("click", () => {
+    if (running) {
+        logLap();
+    } else {
+        resetTimer();
+    }
+});
